08af7ff3ffe9e5944cd53922d8b3e5a235b1dd0e
brunoascenc <brunoascenc1@outlook.com>
2021-03-15 12:55:32 -0300

    collection preview component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
          <Route exact path='/' component={HomePage}/>
+         <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
