59f32c2e1cd198a5de766302c27858d814065380
brunoascenc <brunoascenc1@outlook.com>
2021-03-17 11:25:01 -0300

    storing signup users on firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
 import signInAndSignUp from './pages/signIn-signUp/signIn-signUp';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -20,10 +20,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+
+          console.log(this.state)
+        })
+      }
+
+      this.setState({currentUser: userAuth})
     })
   }
 
--- src/pages/signIn-signUp/signIn-signUp.jsx
+++ src/pages/signIn-signUp/signIn-signUp.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Signin from "../../components/signin/SignIn";
+import SignUp from "../../components/signup/Signup";
+
+import './signIn-signUp.scss'
 
 export default function signInAndSignUp() {
   return (
     <div className="sign-in-and-sign-up">
       <Signin />
+      <SignUp/>
     </div>
   );
 }
