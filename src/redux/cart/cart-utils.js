16da76db47d4b82d98994bfc27ff84a4bc4ff469
brunoascenc <brunoascenc1@outlook.com>
2021-03-20 14:20:20 -0300

    increase and decrease quantity

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -1,10 +1,33 @@
+import Cart from "../../components/cart/Cart";
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
 
-    if(existingCartItem){
-        return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id ?
-            {...cartItem, quantity:cartItem.quantity + 1} : cartItem)
-    }
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
-}
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.add);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItems, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
