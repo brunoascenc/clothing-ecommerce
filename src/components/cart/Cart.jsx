1c952e6e6b5bb4d316455e7298d9f9bfc5a843c2
brunoascenc <brunoascenc1@outlook.com>
2021-03-20 12:51:38 -0300

    items added to the car

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./Cart.scss";
 import CustomButton from "../custom-button/CustomButton";
+import CartItem from "../cart-item/CartItem";
 
-function Cart() {
+function Cart({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 }
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(Cart);
