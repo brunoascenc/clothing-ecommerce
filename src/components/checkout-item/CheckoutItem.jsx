6d512f1a5c0ca71195cf83ab19a6685b859061e1
brunoascenc <brunoascenc1@outlook.com>
2021-03-22 12:31:41 -0300

    redux persist to maitain data

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -37,8 +37,6 @@ const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
   addItem: item => dispatch(addItem(item)),
   removeItem: item => dispatch(removeItem(item))
-  // addItem: (item) = dispatch(addItem(item)),
-  // removeItem: (item) = dispatch(removeItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
