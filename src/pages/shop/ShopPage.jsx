a49256ee66c0a955d5bd7d9a21c7f9f92b129567
brunoascenc <brunoascenc1@outlook.com>
2021-03-24 11:54:40 -0300

    advanced routing and new selectors for collections

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router";
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import Category from "../category/Category";
 // import CollectionsOverview from "../../components/collections-overview/collections-overview";
 
-const ShopPage = () => {
+const ShopPage = ({match}) => {
   return (
     <div className="shop-page">
-       <CollectionsOverview/>
+       <Route exact path ={`${match.path}`} component={CollectionsOverview}/>
+       <Route path={`${match.path}/:collectionId`} component={Category}/>
     </div>
   );
 };
