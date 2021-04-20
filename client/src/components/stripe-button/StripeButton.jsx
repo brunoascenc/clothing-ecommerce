42db283cbff3af2304ed738ad4a46c3b7897b30f
brunoascenc <brunoascenc1@outlook.com>
2021-04-20 15:17:20 -0300

    payment with sripe

--- client/src/components/stripe-button/StripeButton.jsx
+++ client/src/components/stripe-button/StripeButton.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout'
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
-const StripeButton = ({price}) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_51HSKkOGiY20xWn7q37BDBkyBwyzdkjUCZlDaLTuc5KZG44vZpco0q9PLszLRwmoX2vTEBGGBbhDZEgKSRyfUGkzq007qWGkDWS'
-    
-    const onToken = token => {
-        console.log(token)
-        alert('Payment Successful')
-    }
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HSKkOGiY20xWn7q37BDBkyBwyzdkjUCZlDaLTuc5KZG44vZpco0q9PLszLRwmoX2vTEBGGBbhDZEgKSRyfUGkzq007qWGkDWS";
 
-    return (
-        <StripeCheckout
-        label='Pay now'
-        name='CRWN Clothing Ltd.'
-        billingAddress
-        shippingAddress
-        image="https://sendeyo.com/en/f3eb2117da"
-        description={`Your total is $${price}`}
-        amount={priceForStripe}
-        panelLabel='Pay Now'
-        token={onToken}
-        stripeKey={publishableKey}
-        />
-    )
-}
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("payment successful");
+      })
+      .catch((error) => {
+        console.log("payment error:", (error));
+        alert("There was an issu with your payment.");
+      });
+  };
 
-export default StripeButton
+  return (
+    <StripeCheckout
+      label="Pay now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/en/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
